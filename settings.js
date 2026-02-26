7052b820ef1ac71ce1c32c6447beb2670ca26f9d
daudimariange1-ai <daudimariange1@gmail.com>
2026-02-26 12:50:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'daudimariange-ai',
+  author: '‎'daudimariange
+  botName: "daudimariange",
+  botOwner: 'quitebrain', // Your name
+  ownerNumber: '0682899310', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20, 
+  commandMode: "private",
+  maxStoreMessages: 1000000000, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "world class python.",
   version: "3.0.7",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
